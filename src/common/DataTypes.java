9eeddf1b7fc25efe2808e3bd1f8634bef46fd805
Madhuri Tayade <mt202201@nexttechvision.com>
2022-09-13 17:51:34 +0530

    Java data types

---
+++ src/common/DataTypes.java
@@ -0,0 +1,26 @@
+package common;
+
+/**
+ *  specify the different sizes & values that can be stored in the variable
+ *  Types :
+ *  1. Primitive data type
+ *      int,boolean,char,byte,short,int,long,float & double
+ *  2. Non-primitive data type
+ *      Classes,interface & arrays
+ *
+ *  statically-typed language means all variables need to be declared before use.
+ *
+ */
+public class DataTypes {
+    public static void main(String[] args){
+        boolean flag = false;
+        char c = 'A';
+        byte b = 5;
+        short s = 15;
+        int a = 10;
+        long l = 20L;
+
+        float f = 2.5f;
+        double d = 10.5d;
+    }
+}
