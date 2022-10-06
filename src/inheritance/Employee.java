17cc390edb80c00fa8cf571020873a3182af2900
Madhuri Tayade <mt202201@nexttechvision.com>
2022-10-06 17:28:28 +0530

    Single inheritance

---
+++ src/inheritance/Employee.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Employee {
+    int id = 1;
+    String name = "Pooja";
+    float salary = 50000;
+}
+class Programmer extends Employee{
+    float bonus = 5000;
+
+    public static void main(String[] args) {
+        Programmer programmer = new Programmer();
+        float totalSalary = programmer.salary + programmer.bonus;
+        System.out.println("Total Salary of "+programmer.name+" is "+totalSalary);
+    }
+}
