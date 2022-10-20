ed68c14f69ea0303ce2ce8fd2315a33d4eec5969
Madhuri Tayade <mt202202@appystack.com>
2022-10-20 12:21:22 +0530

    abstract class

--- src/abstractclass/Vehicle.java
+++ src/abstractclass/Vehicle.java
@@ -1,25 +1,65 @@
 package abstractclass;
 
 public abstract class Vehicle {
-    abstract void run();
-
-    void getColor() {
-        System.out.println("in get color methpd");
-
+    int wheels;
+    String brand;
+    Vehicle(){}
+    public Vehicle(int wheels, String brand) {
+        this.wheels = wheels;
+        this.brand = brand;
     }
 
+    abstract void run();
+    void getColor() {
+        System.out.println("in get color method");
+    }
+   static void display(){
+       System.out.println("in display method");
+
+   }
+    final void show(){
+        System.out.println("in show method");
+
+    }
 }
 
 class Bike extends Vehicle{
+    @Override
+    void run() {
+        System.out.println("in bike run method");
+    }
+
+    @Override
+    void getColor() {
+        System.out.println("Color is Black");
+    }
+}
+
+class Car extends Vehicle{
 
     @Override
     void run() {
+        System.out.println("in car run method");
+    }
 
+    @Override
+    void getColor() {
+        System.out.println("Color is white");
     }
 }
+
 
 class BikeImpl{
     public static void main(String[] args) {
         Vehicle vehicle = new Bike();
+        vehicle.run();
+        vehicle.getColor();
+        vehicle.show();
+        Vehicle.display();
+
+        Vehicle vehicle1 = new Car();
+        vehicle1.run();
+        vehicle1.getColor();
+        Vehicle.display();
     }
 }
