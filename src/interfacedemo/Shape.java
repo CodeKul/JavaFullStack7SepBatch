d499d755840c9dc0ff656481ea8035163c24bf22
Madhuri Tayade <mt202202@appystack.com>
2022-11-03 17:55:43 +0530

    interface

---
+++ src/interfacedemo/Shape.java
@@ -0,0 +1,28 @@
+package interfacedemo;
+
+public interface Shape extends Drawable{
+   int radius =  50;
+   void calculateArea();
+}
+ interface Drawable{
+    void getDrawable();
+}
+class Circle implements Shape{
+    int radius = 5;
+  @Override
+  public void calculateArea() {
+    double res = 3.14 * radius * radius;
+    System.out.println(res);
+  }
+
+    @Override
+    public void getDrawable() {
+        System.out.println("Circle Object");
+    }
+}
+class ShapeImpl{
+    public static void main(String[] args) {
+        Shape shape = new Circle();
+        shape.calculateArea();
+    }
+}
