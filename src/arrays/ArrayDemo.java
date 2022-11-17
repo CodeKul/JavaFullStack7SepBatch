b6f0e4196312d5f6e6237a64775266d050b9f320
Madhuri Tayade <mt202202@appystack.com>
2022-11-17 17:58:16 +0530

    arrays introduction

--- src/arrays/ArrayDemo.java
+++ src/arrays/ArrayDemo.java
@@ -4,10 +4,13 @@ public class ArrayDemo {
     public static void main(String[] args) {
         int array [] = new int[5];
         System.out.println("Array "+array);
+        array[0] = 10;
+        array[3] = 40;
 
         for(int a:array){
-            System.out.println(a);
+            System.out.print(a+" ");
         }
 
+        System.out.println("4th Position element "+array[3]);
     }
 }
