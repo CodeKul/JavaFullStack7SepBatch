a43b8fe2b3ce191eed5df1a88f89948c1114fbcc
Madhuri Tayade <mt202202@appystack.com>
2022-11-30 18:00:42 +0530

    string introduction

---
+++ src/string/StringDemo.java
@@ -0,0 +1,17 @@
+package string;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        char ch[] = {'W','E','L','C','O','M','E'};
+        System.out.println(ch);
+
+        String s1 = new String("WELCOME");
+        System.out.println(s1);
+
+        String s2 = "WELCOME";
+        System.out.println(s2);
+
+
+        //
+    }
+}
