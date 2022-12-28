7383f06738ad8d461d6cfd37cc3d6a5b1d039b8b
Madhuri Tayade <mt202202@appystack.com>
2022-12-28 17:49:33 +0530

    User Defined Exception

--- src/exceptionhandling/BankImpl.java
+++ src/exceptionhandling/BankImpl.java
@@ -1,5 +1,7 @@
 package exceptionhandling;
 
+import java.util.Scanner;
+
 class SBI{
     double amount = 10000;
 
@@ -16,16 +18,48 @@ class SBI{
         }
     }
 
+    double getBalance(){
+        return amount;
+    }
+
 }
 
 public class BankImpl {
     public static void main(String[] args) {
         SBI sbi = new SBI();
-        sbi.depositAmount(5000);
-        try {
-            sbi.withdraw(20000);
-        }catch (InsufficientBalanceException e){
-            System.out.println(e.getMessage());
-        }
+        Scanner sc = new Scanner(System.in);
+      int ch;
+        do {
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice");
+            ch = sc.nextInt();
+            switch (ch){
+                case 1:
+                    System.out.println("Enter amount for deposit");
+                    double depositAmt = sc.nextDouble();
+                    sbi.depositAmount(depositAmt);
+                    break;
+                case 2:
+                    try {
+                        System.out.println("Enter amount for withdraw");
+                        double withdrawAmt = sc.nextDouble();
+                        sbi.withdraw(withdrawAmt);
+                    }catch (InsufficientBalanceException e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 3:
+                    System.out.println(sbi.getBalance());
+                    break;
+                case 4:System.exit(0);
+                break;
+                default:
+                    System.out.println("Wrong Choice");
+            }
+        }while (ch<=4);
+
     }
 }
