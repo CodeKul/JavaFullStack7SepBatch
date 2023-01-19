25d05935e8851dd5097468aff8d789845e5fd50b
Madhuri Tayade <mt202202@appystack.com>
2023-01-19 17:56:46 +0530

    multithreading introduction

---
+++ src/multithreading/ThreadDemo1.java
@@ -0,0 +1,17 @@
+package multithreading;
+
+public class ThreadDemo1 extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("in run method");
+    }
+
+}
+class ThreadDemoImpl1{
+    public static void main(String[] args) {
+        ThreadDemo1 th1 = new ThreadDemo1(); // born/New
+        System.out.println("in main method");
+        th1.start(); // runnable
+    }
+}
