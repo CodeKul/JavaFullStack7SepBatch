1897ab24a4e6615c40b37b223f84939b876305d6
Madhuri Tayade <mt202202@appystack.com>
2023-01-30 17:59:22 +0530

    multithreading Synchronisation introduction

---
+++ src/multithreading/PrintTable.java
@@ -0,0 +1,44 @@
+package multithreading;
+
+public class PrintTable {
+    void  printTable(int no){
+        for (int i=1;i<=10;i++) {
+            System.out.println(no * i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+class Thread1 extends Thread{
+    PrintTable printTable;
+    Thread1(PrintTable printTable){
+        this.printTable = printTable;
+    }
+    @Override
+    public void run() {
+        printTable.printTable(5);
+    }
+}
+class Thread2 extends Thread{
+    PrintTable printTable;
+    Thread2(PrintTable printTable){
+        this.printTable = printTable;
+    }
+    @Override
+    public void run() {
+        printTable.printTable(3);
+    }
+}
+class PrintTableImpl{
+    public static void main(String[] args) {
+        PrintTable printTable = new PrintTable();
+
+        Thread1 thread1 = new Thread1(printTable);
+        Thread2 thread2 = new Thread2(printTable);
+        thread1.start();
+        thread2.start();
+    }
+}
